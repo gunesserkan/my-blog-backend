6664f21e474a9c6ed4042538e9c5360163822396
gunesserkan <gunesserkan883@gmail.com>
2024-10-30 00:11:54 +0300

    deleted frontend

--- frontend/my-blog/src/container/PageContainer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Container from '@mui/material/Container';
-function PageContainer({ children }) {
-    return (
-        <Container maxWidth='lg' style={{ minHeight: '500px' }}>{children}</Container>
-    )
-}
-
-export default PageContainer
